1d2c363d640e3cf1525731076d8027072a2a4177
Katerina Zamiatina <eozamyatina@gmail.com>
2021-09-13 15:43:48 +0300

    test

--- src/routes.js
+++ src/routes.js
@@ -7,26 +7,11 @@ const HomePage = lazy(() =>
 const HeroPage = lazy(() =>
   import('./pages/HeroPage.jsx' /*webpackChunkName: "hero-page" */),
 );
-// const LoginPage = lazy(() =>
-//   import('./pages/LoginPage' /*webpackChunkName: "login-page" */),
-// );
-
-// const RegistrationPage = lazy(() =>
-//   import('./pages/RegistrationPage' /*webpackChunkName: "registration-page" */),
-// );
-
-// const UserPage = lazy(() =>
-//   import('./pages/UserPage' /*webpackChunkName: "user-page" */),
-// );
-
-// const Page404 = lazy(() =>
-//   import('./pages/Page404' /*webpackChunkName: "404-page" */),
-// );
 
 const routes = [
   {
     exact: true,
-    path: '/',
+    path: '/superheroes-front',
     component: HomePage,
     isProtected: false,
     redirectTo: '/',
@@ -37,34 +22,5 @@ const routes = [
     component: HeroPage,
     isProtected: false,
   },
-  // {
-  //   path: '/user',
-  //   component: UserPage,
-  //   isProtected: true,
-  //   isNav: true,
-  //   redirectTo: '/login',
-  // },
-  // {
-  //   exact: false,
-  //   path: '/login',
-  //   label: 'Login',
-  //   component: LoginPage,
-  //   isProtected: false,
-  //   isLogBar: true,
-  //   redirectTo: '/user',
-  // },
-  // {
-  //   exact: false,
-  //   path: '/register',
-  //   label: 'Registration',
-  //   component: RegistrationPage,
-  //   isProtected: false,
-  //   isLogBar: true,
-  //   redirectTo: '/user',
-  // },
-  // {
-  //   component: Page404,
-  //   isProtected: null,
-  // },
 ];
 export default routes;
