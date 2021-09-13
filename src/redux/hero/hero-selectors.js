40c07548f19f8c6fd170aa5e2cf57a41a2deceda
Katerina Zamiatina <eozamyatina@gmail.com>
2021-09-13 14:50:03 +0300

    test

--- src/redux/hero/hero-selectors.js
+++
@@ -1,1 +0,0 @@
-export const fetchHero = state => state.hero;
--- src/redux/heroes/heroes-selectors.js
+++
@@ -1,6 +0,0 @@
-// import { createSelector } from 'reselect';
-
-export const getStateHeroes = state => state.heroes.heroes;
-export const getisLoading = state => state.heroes.isLoading;
-export const modalAddHero = state => state.heroes.modalAddHero;
-export const getHeroById = state => state.heroes.heroes.heroId;
